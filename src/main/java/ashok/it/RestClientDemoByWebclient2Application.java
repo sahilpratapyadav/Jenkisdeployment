a37b896c913aa4bd06d9e0061f192794f68d81fd
sahilpratapyadav <sahilpratap123yadav@gmail.com>
2022-07-10 15:03:02 +0530

    first commit2

---
+++ src/main/java/ashok/it/RestClientDemoByWebclient2Application.java
@@ -0,0 +1,13 @@
+package ashok.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientDemoByWebclient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientDemoByWebclient2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ashok/it/RestClientDemoByWebclient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ashok.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientDemoByWebclient2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
